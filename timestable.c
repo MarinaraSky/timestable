58d15e35e688b7b5226e24cfd53ab8bb4ded875f
Jack Spence <jack.c.spence@gmail.com>
2018-06-28 18:00:12 -0400

    Added command line arguments table. If statement will check if its in a valid range, 1 <= x <= 32.

--- timestable.c
+++ timestable.c
@@ -1,19 +1,19 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 #define MAX 10
 
 int main(int argc, char *argv[])
 {
-	int header[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
 	if(argc == 1)
 	{
-		for(int i = 0; i < MAX; i++)
+		for(int i = 1; i <= MAX; i++)
 		{
-			if(i == 0)
+			if(i == 1)
 			{
 				printf("\t");
 			}
-			printf("%8d", header[i]);
+			printf("%8d", i);
 		}
 		printf("\n");
 		for(int i = 1; i <= MAX; i++)
@@ -29,7 +29,33 @@ int main(int argc, char *argv[])
 	}
 	if(argc == 2)
 	{
-
+		int new_max = atoi(argv[1]);
+		if(new_max && new_max <= 32 && new_max >= 1)
+		{
+			int new_max = atoi(argv[1]);
+			for(int i = 1; i <= new_max; i++)
+			{
+				if(i == 1)
+				{
+					printf("\t");
+				}
+				printf("%8d", i);
+			}
+			printf("\n");
+			for(int i = 1; i <= new_max; i++)
+			{
+				printf("%8d", i);
+				for(int j = 1; j <= new_max; j++)
+				{
+					printf("%8d", i * j);	
+				}
+				printf("\n");
+			}
+		}
+		else
+		{
+			printf("Invalid selection\n");
+		}
 	}
 	return 0;
 }
