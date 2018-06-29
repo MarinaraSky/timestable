fbdac81ae20eca87f8c408d8284cc95e8d6450b0
Jack Spence <jack.c.spence@gmail.com>
2018-06-28 21:12:42 -0400

    Added argument parsing to check if hex is wanted or not.

--- timestable.c
+++ timestable.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
+#include <string.h>
 #include <stdlib.h>
 
 #define MAX 10
 
 void printTimesTable(int max_number);
+void printTimesTableHex(int max_number);
+void options(int max_number, int in_hex);
 
 int main(int argc, char *argv[])
 {
@@ -11,29 +14,60 @@ int main(int argc, char *argv[])
 	{
 		printTimesTable(MAX);
 	}
-	if(argc == 2)
-	{
-		int new_max = atoi(argv[1]);
-		if(new_max && new_max <= 32 && new_max >= 1)
-		//if checks that new_max is a number and between 1 and 32
+	// Checks are designed to print default table if arguments are wrong
+	else if(argc > 1 && argc < 4) //checks if there are enough arguments
+	{ // this block parses arguments between max and if -h is enabled
+		int max_number = MAX;
+		if(atoi(argv[1]) != 0)
 		{
-			printTimesTable(new_max);
+			max_number = atoi(argv[1]);
 		}
-		else //catches if letters or numbers out of range are entered
+		else if(argc > 2  && atoi(argv[2]) != 0 )
 		{
-			printf("Invalid selection\n");
+			max_number = atoi(argv[2]);
+		}
+		int in_hex = 0;
+		for(int i = 0; i < argc; i++)
+		{
+			if(strcmp(argv[i], "-h") == 0)
+			{
+				in_hex = 1;
+			}
+		}
+		//options will wall the print functions
+		options(max_number, in_hex);
+	}
+	else //catches if letters or numbers out of range are entered
+	{
+		printTimesTable(MAX);
+	}
+	return 0;
+}
+
+void options(int max_number, int in_hex)
+{
+	if((max_number && max_number <= 32 && max_number >= 1) || (in_hex == 1))
+	//if checks that new_max is a number and between 1 and 32
+	{
+		if(in_hex == 0)
+		{
+			printTimesTable(max_number);
+		}
+		else
+		{
+			printTimesTableHex(max_number);
 		}
 	}
 	else
 	{
-		printf("Invalid selection\n");
+		printTimesTable(MAX);
 	}
-	return 0;
 }
 
 void printTimesTable(int max_number)
 {
 	//Nested for loops to print times tables
+	printf("\t%s - Up to %d\n", "DEFAULT TABLE", max_number);
 	for(int i = 1; i <= max_number; i++)
 	{
 		if(i == 1)
@@ -61,6 +95,42 @@ void printTimesTable(int max_number)
 		for(int j = 1; j <= max_number; j++)
 		{
 			printf("%8d", i * j);
+		}
+		printf("\n");
+	}
+}
+
+void printTimesTableHex(int max_number)
+{
+	//Nested for loops to print times tables
+	printf("\t%s - Up to %d\n", "HEX TABLE", max_number);
+	for(int i = 1; i <= max_number; i++)
+	{
+		if(i == 1)
+		{
+			printf("\t");
+		}
+		printf("%8x", i);
+	}
+	printf("\n");
+	for(int i = 0; i <= max_number; i++)
+	{
+		if(i != 0)
+		{
+			printf("%8s", "********");
+		}
+		else
+		{
+			printf("\t");
+		}
+	}
+	printf("\n");
+	for(int i = 1; i <= max_number; i++)
+	{
+		printf("%7x|", i);
+		for(int j = 1; j <= max_number; j++)
+		{
+			printf("%8x", i * j);
 		}
 		printf("\n");
 	}
